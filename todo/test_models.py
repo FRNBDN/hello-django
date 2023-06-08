3250417559d70d2e3766c2f08951c6e4c7a3edf7
FRNBDN <francesco.badini94@gmail.com>
2023-06-09 00:52:56 +0900

    install coverage, add tests for full coverage

--- todo/test_models.py
+++ todo/test_models.py
@@ -6,3 +6,7 @@ class TestModels(TestCase):
     def test_done_defaults_to_fales(self):
         item = Item.objects.create(name='test item')
         self.assertFalse(item.done)
+    
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='test item')
+        self.assertEqual(str(item),'test item')
